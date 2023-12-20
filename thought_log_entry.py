7deb4c6d801c331fbbc74e4585ae648c904b60dc
Colin Mason <masonc789@gmail.com>
2023-12-20 06:41:13 -0500

    add script command boilerplate

---
+++ thought_log_entry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title thought log entry
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📓
+# @raycast.argument1 { "type": "text", "placeholder": "thought log entry" }
+
+# Documentation:
+# @raycast.description Adds a timestamped entry to thought log text file.
+# @raycast.author masonc789
+# @raycast.authorURL https://raycast.com/masonc789
