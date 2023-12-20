1cd5ac75dbb40763251f87b4895716252426b58d
Colin Mason <masonc789@gmail.com>
2023-12-20 06:41:56 -0500

    add entry formatting logic

--- thought_log_entry.py
+++ thought_log_entry.py
@@ -13,3 +13,19 @@
 # @raycast.description Adds a timestamped entry to thought log text file.
 # @raycast.author masonc789
 # @raycast.authorURL https://raycast.com/masonc789
+
+import sys
+from datetime import datetime
+import os
+
+# Constants
+LOG_FILE_PATH = "/Users/colin/Dropbox (Maestral)/Daily Notes/thought log.txt"
+
+# Get the current date and time
+current_date = datetime.now().strftime("%-m-%d-%y")
+current_time = datetime.now().strftime("%-I:%M %p")
+header = f"{current_date}\n---\n"
+
+# Get the entry from the argument
+entry = sys.argv[1].strip()
+formatted_entry = f"{current_time} - {entry}\n"
